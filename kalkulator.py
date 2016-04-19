d191edb35b7accdeebfb1e98948a6d95f6ecae26
mateuszgreb <grebus@gmail.com>
2016-04-19 15:22:29 +0200

    Dodano plik kalkulator.py z funkcja

---
+++ kalkulator.py
@@ -0,0 +1,10 @@
+def main():
+    suma = 0
+    for i in range(0,5):
+        wyraz = input()
+        if wyraz[0].isupper():
+            suma = suma +1
+    return suma
+
+if __name__ == '__main__':
+    print(main())
